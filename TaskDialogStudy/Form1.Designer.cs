3af70f4623b63a21ae96363458e987455374bf20
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-11-19 05:03:14 -0800

    Basic scaffold

--- TaskDialogStudy/Form1.Designer.cs
+++ TaskDialogStudy/Form1.Designer.cs
@@ -28,20 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(300, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Simple task dialog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(3, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Simple task dialog with custom buttons";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(3, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Simple task dialog with footer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(3, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Simple task dialog with command links";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(3, 125);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 25);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Simple self-closing task dialog";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Location = new System.Drawing.Point(3, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 25);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Multi-page task dialog";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(390, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- TaskDialogStudy/Form1.cs
+++ TaskDialogStudy/Form1.cs
@@ -17,5 +17,76 @@ namespace TaskDialogStudy
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1. Create a simple task dialog
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 2. Create a simple task dialog with custom buttons
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 3. Create a simple task dialog 
+            //    a. a custom content,
+            //    b. a verification text, and
+            //    c. a footer with a help link (e.g. go to https://dot.net/)
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 4. Create a task dialog with command link buttons
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // 5. Create a simple self-closing task dialog with a progress bar
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // 6. Implement a multi page task dialog:
+            //    a.  "Yes" button is enabled after checkbox is checked
+            //    b. Progress bar initialises as marquee for few seconds, then runs from 0 to 100%
+            //    c. Show results command link button
+
+            var taskDialog = new TaskDialog();
+
+            // write code here...
+
+            taskDialog.ShowDialog(this);
+        }
     }
 }
